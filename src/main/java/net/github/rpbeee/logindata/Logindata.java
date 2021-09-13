a3bab3a156ac0aa1270ca578addfc4696c1f6176
RPbeee <sirakami.65536@gmail.com>
2021-09-13 20:50:56 +0900

    Initial commit

---
+++ src/main/java/net/github/rpbeee/logindata/Logindata.java
@@ -0,0 +1,77 @@
+package net.github.rpbeee.logindata;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Date;
+
+public final class Logindata extends JavaPlugin {
+    // プレイヤー宛メッセージのプレフィックス
+    String premsg = ChatColor.GREEN+"[Logindata] "+ChatColor.WHITE;
+
+    @Override
+    public void onEnable() {
+        getLogger().info("ログインデータPL開始");
+        getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ログインデータPL終了");
+    }
+
+    public void argserror(CommandSender sender, boolean ecode) {
+        if(ecode){
+            sender.sendMessage(premsg+"引数が多いか足りません！");
+        } else {
+            sender.sendMessage(premsg+"引数が間違っています！");
+        }
+        sender.sendMessage(premsg+"/logindata firstjoin [プレイヤー名]");
+        sender.sendMessage(premsg+"/logindata lastjoin [プレイヤー名]");
+        sender.sendMessage(premsg+"/logindata count [プレイヤー名]");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        if (args.length != 2) {
+            // too less or much args.
+            argserror(sender, true);
+            return true;
+        }
+
+        Player player = Bukkit.getServer().getPlayer(args[1]);
+        if (player==null) {
+            sender.sendMessage(premsg+"プレイヤー名が間違っています");
+            return true;
+        }
+        if (!args[1].equals(player.getName()) || !player.hasPlayedBefore()) {
+            sender.sendMessage(premsg+"プレイヤー名が間違っています");
+            return true;
+        }
+
+        CustomYML cyml = new CustomYML(this, "count.yml");
+        cyml.saveDefaultConfig();
+        cyml.reloadConfig();
+        FileConfiguration lyml = cyml.getConfig();
+
+        if(args[0].equalsIgnoreCase("firstjoin")) {
+            Date date = new Date(player.getFirstPlayed());
+            sender.sendMessage(premsg+args[1]+" の初ログイン日時は: "+date.toLocaleString()+" です");
+        } else if(args[0].equalsIgnoreCase("lastjoin")) {
+            Date date = new Date(player.getLastPlayed());
+            sender.sendMessage(premsg+args[1]+" の最終ログイン日時は: "+date.toLocaleString()+" です");
+        } else if(args[0].equalsIgnoreCase("count")) {
+            int count = lyml.getInt(args[1]+".count");
+            sender.sendMessage(premsg+args[1]+" の累計ログイン回数は: "+count+" 回です");
+        } else {
+            argserror(sender, false);
+        }
+        cyml.saveConfig();
+        return true;
+    }
+}
---
+++ src/main/java/net/github/rpbeee/logindata/PlayerJoin.java
@@ -0,0 +1,24 @@
+package net.github.rpbeee.logindata;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+
+public class PlayerJoin implements Listener {
+    @EventHandler
+    public void pjoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        Plugin plugin = player.getServer().getPluginManager().getPlugin("Logindata");
+        CustomYML cyml = new CustomYML(plugin, "count.yml");
+        cyml.saveDefaultConfig();
+        cyml.reloadConfig();
+        FileConfiguration lyml = cyml.getConfig();
+        int count = lyml.getInt(player.getName()+".count");
+        lyml.set(player.getName()+".count", count+1);
+        cyml.saveConfig();
+        player.getServer().getLogger().info(player.getName()+" のログインを確認しました");
+    }
+}
